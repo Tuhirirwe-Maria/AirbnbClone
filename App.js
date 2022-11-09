c94889dc65d3e110794a854421f6950a83297575
Tuhirirwe Maria <tuhirirwemaria@gmail.com>
2022-11-09 20:03:41 +0300

    Add props to the post

--- App.js
+++ App.js
@@ -12,12 +12,15 @@ import {
 } from 'react-native';
 import HomeScreen from './src/screens/Home'
 import Post from './src/components/Post'
+import feed from './assets/data/feed';
 
+const post1 = feed[0];
+const post2 = feed[1]
 const App = () =>{
   return(
     <View>
         {/* <HomeScreen/> */}
-        <Post/>
+        <Post post = {post2 }/>
     </View>
   )
 }
--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -2,21 +2,28 @@ import React from "react";
 import {View, Text, Image} from 'react-native'
 import styles from "./styles";
 
-const Post = () => {
+const Post = (props) => {
+
+    const post = props.post
+
     return(
         <View style = {styles.container }>
-            <Image source={{uri: 'https://res.cloudinary.com/awesomeone-maria/image/upload/v1667819402/Airbnb/bed_o7fsr3.jpg'}} style = {styles.image} />
+            <Image source={{uri: post.image}} style = {styles.image} />
             {/* title */}
-            <Text style = {styles.postTitle}>1 bed 1 bedroom</Text>
+            <Text style = {styles.postTitle}>{post.bed} {post.bedroom} bedroom</Text>
             {/* description */}
-            <Text style = {styles.postDescription}>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque   </Text>
+            <Text style = {styles.postDescription} >
+               {post.type}. 
+               {post.title}
+                </Text>
             {/* price */}
             <Text style = {styles.postPrice}>
-            <Text style = {styles.postOldPrice}>$46 </Text>
-            <Text style = {styles.postNewPrice}>$36 / night</Text>
+            <Text style = {styles.postOldPrice}>${post.oldPrice}</Text>
+
+            <Text style = {styles.postNewPrice}>  ${post.newPrice}/night</Text>
             </Text>
             {/* total price */}
-            <Text style = {styles.postTotalPrice}>$230 total </Text>
+            <Text style = {styles.postTotalPrice}>${post.totalPrice} total </Text>
         </View>
     )
 }
